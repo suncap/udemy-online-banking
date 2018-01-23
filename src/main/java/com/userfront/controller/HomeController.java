5701f96b2b44bd059169ccc084cba590d342653b
Unknown <suncap11@gmail.com>
2018-01-23 21:07:31 +0200

    UserFront page and Savings, Primary account

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.userfront.controller;
 
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userfront.dao.RoleDao;
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.User;
 import com.userfront.domain.security.UserRole;
 import com.userfront.service.UserService;
@@ -61,5 +64,17 @@ public class HomeController {
 			userService.createUser(user, roles);
 			return "redirect:/";
 		}
+	}
+	
+	@RequestMapping("/userFront")
+	public String userFront(Principal principal, Model model) {
+		User user = userService.findByUsername(principal.getName());
+		PrimaryAccount primaryAccount = user.getPrimaryAccount();
+		SavingsAccount savingsAccount = user.getSavingsAccount();
+		
+		model.addAttribute("primaryAccount", primaryAccount);
+		model.addAttribute("savingsAccount", savingsAccount);
+
+		return "userFront";
 	}
 }
