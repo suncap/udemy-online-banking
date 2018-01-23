ba2932bcb9f7764a744aeeda36e5cb1693b33555
Unknown <suncap11@gmail.com>
2018-01-23 21:34:07 +0200

    Primary and Savings account controller

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -1,20 +1,33 @@
 package com.userfront.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userfront.domain.User;
+import com.userfront.service.UserService;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
+	@Autowired
+	private UserService userService;
+
 	@RequestMapping("/primaryAccount")
-	public String primaryAccount() {
+	public String primaryAccount(Principal principal, Model model) {
+		User user = userService.findByUsername(principal.getName());
+		model.addAttribute("primaryAccount", user.getPrimaryAccount());
 		return "primaryAccount";
 	}
-	
 
 	@RequestMapping("/savingsAccount")
-	public String savingsAccount() {
+	public String savingsAccount(Principal principal, Model model) {
+		User user = userService.findByUsername(principal.getName());
+		model.addAttribute("savingsAccount", user.getSavingsAccount());
 		return "savingsAccount";
 	}
 }
