4e8d51261aa6496835d35d3e3afbbf28a659321d
Unknown <suncap11@gmail.com>
2018-02-06 00:33:16 +0200

    Login services and user inteface

--- AdminPortal/AdminPortal/src/app/app.module.ts
+++ AdminPortal/AdminPortal/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing }  from './app.routing';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+
+import { LoginService } from './login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [
+      LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
